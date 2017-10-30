783d411138eb9d3533049c9f9af90a4ca1270d59
Liu <jl25@illinois.edu>
2017-10-30 14:36:08 -0500

    Add basic audio effect

--- Assets/Script/Avatar.cs
+++ Assets/Script/Avatar.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon;
 
+[RequireComponent(typeof(AudioSource))]
 public class Avatar : Photon.PunBehaviour {
 
 	private GameObject frisbee;
 	private Transform right_hand;
+
+	public AudioClip audio_creat;
+	public AudioClip audio_throw;
+	public AudioClip audio_catch;
 
 	void Start () {
 		foreach (Transform t in GetComponentsInChildren<Transform>()) {
@@ -22,12 +27,14 @@ public class Avatar : Photon.PunBehaviour {
 		if (OVRInput.GetUp(OVRInput.RawButton.RIndexTrigger) && !frisbee) {
 			frisbee = PhotonNetwork.Instantiate("Frisbee", Vector3.zero, Quaternion.identity, 0);
 			frisbee.GetComponent<Frisbee>().SetOwner(right_hand);
+			AudioSource.PlayClipAtPoint(audio_creat, right_hand.position);
 		}
 
 		// release the frisbee when A button released
 		if (OVRInput.GetUp(OVRInput.Button.One) && frisbee) {
 			frisbee.GetComponent<Frisbee>().SetOwner(null);
 			frisbee = null;
+			AudioSource.PlayClipAtPoint(audio_throw, right_hand.position);
 		}
 	}
 
@@ -36,6 +43,7 @@ public class Avatar : Photon.PunBehaviour {
 			frisbee = other.gameObject;
 			frisbee.GetComponent<Frisbee>().SetOwner(right_hand);
 			frisbee.GetComponent<PhotonView>().RequestOwnership();
+			AudioSource.PlayClipAtPoint(audio_catch, right_hand.position);
 		}
 	}
 }
